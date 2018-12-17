aef728854bd38141d266a7d2422a507b572d9aaa
Yhyo <youkharta@gmail.com>
2018-12-17 17:59:10 +0100

    added simple movement to ai paddle

--- pong.js
+++ pong.js
@@ -1,5 +1,14 @@
 const $pong = $("#pong");
 const $playerPaddle = $("#p-paddle");
+const $aiPaddle = $("#ai-paddle");
+const $ball = $("#ball");
+
+const aiPaddle={
+    speed:5,
+    dir:1,
+    top:0
+
+}
 
 $pong.mousemove(function(evt){
     const top = Math.min(
@@ -9,6 +18,19 @@ $pong.mousemove(function(evt){
 
 
 });
+function update(){
+    aiPaddle.top+=aiPaddle.dir*aiPaddle.speed;
+    if( aiPaddle.top>$pong.height()-$aiPaddle.height() ||
+        aiPaddle.top<0
+    ){
+        aiPaddle.dir*=-1;
+    }
+    $aiPaddle.css({
+        top: `${aiPaddle.top}px`
+    });
+}
+
+setInterval(update,1000/30);
 
 function setX(X){
     $playerPaddle.css({
